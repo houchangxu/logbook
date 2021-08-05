886c1a5c8697bdfdb9a21099bf872111873257ec
houchangxu <79292522@qq.com>
2021-08-05 21:07:52 +0800

    commit

--- logbook/src/main/java/com/logbook/LogbookApplication.java
+++ logbook/src/main/java/com/logbook/LogbookApplication.java
@@ -3,15 +3,21 @@ package com.logbook;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan("com.logbook.*.mapper")
 @ComponentScan(basePackages = {"com.logbook.config","com.logbook.business","com.logbook.user"})
-public class LogbookApplication {
+public class LogbookApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LogbookApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(LogbookApplication.class);
+	}
 }
